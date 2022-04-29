81a3f142d7eb14988298fdbc6539e16f93dad9a0
Lucas Barga <lucas.barga.ext@dasa.com.br>
2022-04-29 03:42:09 -0300

    sls whithdout db

--- handler.js
+++ handler.js
@@ -1,16 +1,68 @@
-"use strict";
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
-module.exports.hello = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message:
-                    "Go Serverless v3.0! Your function executed successfully!",
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
-};
+const users = [
+    {
+        id: "1",
+        name: "1",
+    },
+    {
+        id: "2",
+        name: "2",
+    },
+    {
+        id: "3",
+        name: "3",
+    },
+];
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+app.post("/users", function (req, res) {
+    users.push(req.body);
+    res.sendStatus(200);
+});
+
+app.get("/users/:id", function (req, res) {
+    const { id } = req.params;
+    const user = users.find((user) => user.id === id);
+
+    res.status(200).send(user);
+});
+
+app.put("/users/:id", function (req, res) {
+    const { id } = req.params;
+    const { name } = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex) {
+        users.splice(userIndex, 1);
+        users.push({
+            id,
+            name,
+        });
+    }
+
+    res.sendStatus(200);
+});
+
+app.delete("/users/:id", function (req, res) {
+    const { id } = req.params;
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+        ``;
+        users.splice(userIndex, 1);
+    }
+    res.sendStatus(200);
+});
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        error: "Not Found",
+    });
+});
+
+module.exports.handler = serverless(app);
